5190991402a1df83191fc6572a748c14763a3f6b
Christian77777 <Christian77777@WINDCOMP>
2018-09-21 17:54:52 -0700

    [1.0.0] Project Upload

---
+++ src/main/java/com/Christian77777/Frame_Summoner/StreamGobbler.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2018 Christian Devile
+ * 
+ * This file is part of FoxTrotUpscaler.
+ * 
+ * FoxTrotUpscaler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * FoxTrotUpscaler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with FoxTrotUpscaler. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.Christian77777.Frame_Summoner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class StreamGobbler extends Thread
+{
+
+	private final InputStream is;
+	private final boolean post;
+
+	public StreamGobbler(InputStream is, boolean post)
+	{
+		this.is = is;
+		this.post = post;
+	}
+
+	public void run()
+	{
+		try
+		{
+			InputStreamReader isr;
+			isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String line = null;
+			while ((line = br.readLine()) != null)
+			{
+				line.trim();
+				if (post)
+					System.out.println(line);
+			}
+		}
+		catch (IOException ioe)
+		{
+			ioe.printStackTrace();
+			System.exit(20);
+		}
+	}
+}
